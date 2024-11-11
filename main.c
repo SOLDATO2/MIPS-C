a6e2f48ebb4271d94cae66cde9ae99564bb8d135
Fe <felipeporto553@gmail.com>
2024-11-11 18:26:16 -0300

    add tipo J

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 //comandos
 //R ( ADD (OK), SUB (OK), AND (OK) )
 //I (ADDI (OK), SUBI (ok), LI (ok) )
-//J (J (n ok), JR (n ok), JAL(n ok) )
+//J (J (OK), JR (OK), JAL (OK) )
 
 
 struct PC {
@@ -26,11 +26,13 @@ struct Instrucao {
 void criarRegistradores(struct PC *pc){
     //inicia todos os registradores com 0, vamos armazenar os valores dos registradores aqui
     for(int i = 0; i < REGISTRADORES; i++){
-        pc->vetor_registradores[i] = 0;
+        pc->vetor_registradores[i] = 1;
     }
     pc->pc_valor = 0; //inicia o pc em 0
+
 }
 int regIndice(char *registrador) {
+    //https://melted-ray-4c3.notion.site/Aula-04-Aprofundamento-em-Assembly-MIPS-2c1510d6c9754cbea48e2b236ab4aeaf
     //retorna o indice do registrador mapeado
 
     return (strcmp(registrador, "$zero") == 0) ? 0 :
@@ -67,13 +69,17 @@ int regIndice(char *registrador) {
            (strcmp(registrador, "$ra") == 0) ? 31 : -1;
 }
 
+
 void lerInstrucao(char *buffer, struct Instrucao *instrucao){
+    //https://melted-ray-4c3.notion.site/Aula-04-Aprofundamento-em-Assembly-MIPS-2c1510d6c9754cbea48e2b236ab4aeaf
+
     char rt[10], rs[10], rd[10];
     int imediato;
 
-    //ADDI e SUBI
-    sscanf(buffer, "%s", instrucao->instrucao);
 
+    
+    sscanf(buffer, "%s", instrucao->instrucao);
+    //ADDI e SUBI
     if (strncmp(instrucao->instrucao, "ADDI", 4) == 0 || strncmp(instrucao->instrucao, "SUBI", 4) == 0) {
         if (sscanf(buffer, "%s %[^,], %[^,], %d", instrucao->instrucao, rd, rs, &imediato) == 4) {
             instrucao->rd = regIndice(rd);
@@ -95,11 +101,38 @@ void lerInstrucao(char *buffer, struct Instrucao *instrucao){
         instrucao->rd = regIndice(rd);
         instrucao->rs = regIndice(rs);
         instrucao->rt = regIndice(rt);
+        
         return;
     }
 
+    //J
+    if (strncmp(instrucao->instrucao, "J", 2) == 0){
+        if (sscanf(buffer, "%s %d", instrucao->instrucao, &imediato) == 2){
+            instrucao->imediato = imediato;
+            return;
+        }
+    }
+    //JAL
+    if (strncmp(instrucao->instrucao, "JAL", 2) == 0){
+        if (sscanf(buffer, "%s %d", instrucao->instrucao, &imediato) == 2){
+            instrucao->imediato = imediato;
+            return;
+        }
+
+    }
+    //JR
+    if (strncmp(instrucao->instrucao, "JR", 2) == 0){
+        if (sscanf(buffer, "%s %s", instrucao->instrucao, rs) == 2){
+            instrucao->rs = regIndice(rs);
+            return;
+        }
+    }
+
+
+
     printf("Instrucao nao reconhecida\n");
 }
+
 
 void mostrarRegistradores(struct PC *pc){
     for(int i = 0; i < REGISTRADORES; i++){
@@ -134,6 +167,23 @@ void LI(struct Instrucao *instrucao, struct PC *pc){
 
 void SUBI(struct Instrucao *instrucao, struct PC *pc){
     pc->vetor_registradores[instrucao->rs] = pc->vetor_registradores[instrucao->rd] - instrucao->imediato;
+}
+
+
+//--------------------------------------
+//INSTRUCOES TIPO J
+
+void J(struct Instrucao *instrucao, struct PC *pc){
+    pc->pc_valor = instrucao->imediato;
+}
+
+void JAL(struct Instrucao *instrucao, struct PC *pc){
+    pc->vetor_registradores[31] = pc->pc_valor + 4; //acessando o index direto aqui pq $ra é um registrador "escondido"
+    pc->pc_valor = instrucao->imediato;
+}
+
+void JR(struct Instrucao *instrucao, struct PC *pc){
+    pc->pc_valor = pc->vetor_registradores[instrucao->rs];
 }
 
 //--------------------------------------
@@ -171,55 +221,98 @@ void executarInstrucao(struct Instrucao *instrucao, struct PC *pc){
     }
     //--------------------------------------
 
+    //TIPO J
+    if(strcmp(instrucao->instrucao, "J") == 0){
+        J(instrucao, pc);
+        return;
+    }
+
+    if(strcmp(instrucao->instrucao, "JAL") == 0){
+        JAL(instrucao, pc);
+        return;
+    }
+
+    if(strcmp(instrucao->instrucao, "JR") == 0){
+        JR(instrucao, pc);
+        return;
+    }
+    //--------------------------------------
+
     printf("Nenhuma instrucao executada pois nao foi possivel identificar o comando\n");
 }
 
 void mostrarEstruturaInstrucao(struct Instrucao *instrucao){
-    // Verifica se é uma instrução do tipo R
     if(strcmp(instrucao->instrucao, "ADD") == 0 || strcmp(instrucao->instrucao, "SUB") == 0 || strcmp(instrucao->instrucao, "AND") == 0) {
 
         printf("Tipo R: Opcode: 0, rs: %d, rt: %d, rd: %d, shamt: 0, funct: ", instrucao->rs, instrucao->rt, instrucao->rd);
         
         if(strcmp(instrucao->instrucao, "ADD") == 0)
-            printf("32");
+            printf("32\n");
         else if(strcmp(instrucao->instrucao, "SUB") == 0)
-            printf("34");
+            printf("34\n");
         else if(strcmp(instrucao->instrucao, "AND") == 0)
-            printf("36");
+            printf("36\n");
         return;
     }
 
-    // Verifica se é uma instrução do tipo I
     if(strcmp(instrucao->instrucao, "ADDI") == 0 || strcmp(instrucao->instrucao, "SUBI") == 0 || strcmp(instrucao->instrucao, "LI") == 0) {
+
         printf("Tipo I: Opcode: ");
+        
         if(strcmp(instrucao->instrucao, "ADDI") == 0)
             printf("8");
         else if(strcmp(instrucao->instrucao, "SUBI") == 0)
-            printf("10");
+            printf("8");
         else if(strcmp(instrucao->instrucao, "LI") == 0)
-            printf("11");
-
-        printf(", rs: %d, rd: %d, imediato: %d", instrucao->rs, instrucao->rd, instrucao->imediato);
+            printf("8");
+        
+        printf("rs: %d, rt: %d, imediato: %d\n", instrucao->rs, instrucao->rd, instrucao->imediato);
         return;
     }
 
-    printf("Instrucao nao reconhecida.\n");
+    if(strcmp(instrucao->instrucao, "J") == 0 || strcmp(instrucao->instrucao, "JAL") == 0 || strcmp(instrucao->instrucao, "JR") == 0) {
+        
+        printf("Tipo J: Opcode: ");
+        
+        if(strcmp(instrucao->instrucao, "J") == 0)
+            printf("2");
+        else if(strcmp(instrucao->instrucao, "JAL") == 0)
+            printf("3");
+        else if(strcmp(instrucao->instrucao, "JR") == 0)
+            printf("0");
+        
+        if(strcmp(instrucao->instrucao, "JR") == 0)
+            printf("rs: %d\n", instrucao->rs);
+        else
+            printf("imediato: %d\n", instrucao->imediato);
+        return;
+    }
+
+    printf("Instrução não reconhecida\n");
 }
 
-int main(){
-    char instrucao[60];
-    struct PC pc;
-    struct Instrucao inst;
 
+
+int main(){
+    
+    struct PC pc;
+    struct Instrucao instrucao;
+    char buffer[50];
     criarRegistradores(&pc);
 
     while(1){
-        printf("\nDigite a instrucao: ");
-        fgets(instrucao, sizeof(instrucao), stdin);
-        instrucao[strcspn(instrucao, "\n")] = 0; // Remove o \n do final da string
+        printf("Digite um comando: ");
+        
+        fgets(buffer, sizeof(buffer), stdin); //le e armazena comando mips no buffer
 
-        lerInstrucao(instrucao, &inst);
-        executarInstrucao(&inst, &pc);
+        lerInstrucao(buffer, &instrucao);
+
+        mostrarEstruturaInstrucao(&instrucao);
+
+        executarInstrucao(&instrucao, &pc);
+        
+        
+        
         mostrarRegistradores(&pc);
     }
 
